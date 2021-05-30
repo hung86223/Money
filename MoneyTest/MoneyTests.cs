97eeadadca8e4adfd7d1d3e5004b49a83613bd69
ben.huang52 <ben.huang52@titansoft.com.sg>
2021-05-30 15:43:14 +0800

    complete test TestEquality

--- MoneyTest/MoneyTests.cs
+++ MoneyTest/MoneyTests.cs
@@ -18,6 +18,13 @@ namespace MoneyTest
             product = five.Times(3);
             Assert.AreEqual(15,product.amount);
         }
+
+        [Test]
+        public void TestEquality()
+        {
+            Assert.True(new Dollar(5).Equal(new Dollar(5)));
+            Assert.False(new Dollar(5).Equal(new Dollar(6)));
+        }
     }
 
     public class Dollar
@@ -32,6 +39,11 @@ namespace MoneyTest
         public Dollar Times(int multiplier)
         {
             return new(amount*multiplier);
+        }
+
+        public bool Equal(Dollar dollar)
+        {
+            return amount == dollar.amount;
         }
     }
 }
